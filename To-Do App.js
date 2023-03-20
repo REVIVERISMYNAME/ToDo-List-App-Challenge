a8572122ba5621557d48294f74e56cfca66b87ec
Mac <115026186+REVIVERISMYNAME@users.noreply.github.com>
2023-03-20 17:16:50 +0200

    Second commit

--- To-Do App.js
+++ To-Do App.js
@@ -1,1 +1,45 @@
 console.log("JS-Page Loaded");
+
+// Heading Date // 
+const dateElement = document.getElementById("date");
+const options = {weekday : "long", month:"short", day:"numeric"};
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+
+//Created Array displayed in list// 
+var taskList = ["Task-1, Task-2"];
+console.log(taskList);
+
+    var li = document.createElement("li"); 
+        li.textContent = taskList;
+
+     console.log(li);
+     var list  = document.getElementById("list"); 
+         list.appendChild(li);
+         console.log(li);
+
+
+
+
+
+
+
+var addButton = document.getElementById("addButton");
+    addButton.addEventListener("click", addTask);
+    function addTask() {
+        console.log(" -- addButton Pressed -- ");
+        console.log(listOfTasks);
+    
+        
+        var inputField = document.getElementById("inputField").value;
+       
+        var listOfTasks = [];
+            listOfTasks.push("")
+            
+        var li = document.createElement("li");
+            li.textContent = listOfTasks;
+
+            var list  = document.getElementById("list"); 
+            list.appendChild(li); 
+    }
